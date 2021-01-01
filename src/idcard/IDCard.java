6ddc79c190d186da60c93c3167c41b429e469ae2
nayoung <kny8493@naver.com>
2021-01-02 00:36:02 +0900

    [Factory Method] IDCard클래스에 인증번호 붙이고, IDCardFactory 클래스가 인증번호와 소지자의 대응표를 갖도록

--- src/idcard/IDCard.java
+++ src/idcard/IDCard.java
@@ -3,17 +3,22 @@ import framework.*;
 
 public class IDCard extends Product {
     private String owner;
+    private int serial;
 
-    public IDCard(String owner) {
-        System.out.print(owner + "카드 만듦\n");
+    public IDCard(String owner, int serial) {
+        System.out.print("["+serial+"]"+owner + "카드 사용\n");
         this.owner = owner;
+        this.serial = serial;
     }
     @Override
     public void use() {
-        System.out.print(owner + "카드 사용\n");
+        System.out.print("["+serial+"]"+owner + "카드 사용\n");
     }
 
     public String getOwner() {
         return owner;
+    }
+    public int getSerial() {
+        return serial;
     }
 }
--- src/idcard/IDCardFactory.java
+++ src/idcard/IDCardFactory.java
@@ -3,18 +3,28 @@ import framework.*;
 import java.util.*;
 
 public class IDCardFactory extends Factory {
+    private HashMap db = new HashMap();
+    private int serial=100;
+
     private List owners = new ArrayList();
 
     @Override
+    // synchronized -
     protected Product createProduct(String owner) {
-        return new IDCard(owner);
+        return new IDCard(owner, serial++);
     }
 
     @Override
     protected void registerProduct(Product product) {
+        IDCard card = (IDCard) product;
+        db.put(new Integer(card.getSerial()), card.getOwner());
+
         owners.add(((IDCard)product).getOwner());
     }
     public List getOwners() {
         return owners;
+    }
+    public HashMap getDb() {
+        return db;
     }
 }
