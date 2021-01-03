c58e57f2d9cf57fabc41f12029b42c2798642677
nayoung <kny8493@naver.com>
2021-01-04 01:01:20 +0900

    주석 추가

--- src/FactoryMethod/framework/Factory.java
+++ src/FactoryMethod/framework/Factory.java
@@ -1,7 +1,9 @@
 package FactoryMethod.framework;
 
-import FactoryMethod.idcard.IDCard;
-
+// import FactoryMethod.idcard.IDCard;
+// Template Method 패턴이 사용되는 class
+// 제품을 실제로 만들고 등록하는 구현은 하위클래스에서 함
+// Tempplte Method 패턴 - 하위 클래스에서 구체적으로 처리하는 패턴
 public abstract class Factory {
     public final Product create(String owner) {
         Product p = createProduct(owner);
@@ -9,12 +11,12 @@ public abstract class Factory {
         registerProduct(p);
         return p;
     }
-    // 자바의 특징 -> 접근지정자 (protected, public, private) => 캡슐화
-    // createProduct : 인스턴스 생성을 위한 메소드
-    // new로 실제 인스턴스를 생성하는 대신 인스턴스 생성을 위한 메소드를 호출해서 구체적인 클래스 이름에 의한 속박에서 상위 클래스를 자유롭게함
-    protected Product createNew (String owner) {
-        return new IDCard(owner);
-    }
+//     자바의 특징 -> 접근지정자 (protected, public, private) => 캡슐화
+//     createProduct : 인스턴스 생성을 위한 메소드
+//     new로 실제 인스턴스를 생성하는 대신 인스턴스 생성을 위한 메소드를 호출해서 구체적인 클래스 이름에 의한 속박에서 상위 클래스를 자유롭게함
+//    protected Product createNew (String owner) {
+//        return new IDCard(owner);
+//    }
     protected abstract Product createProduct(String owner);
     protected abstract void registerProduct(Product product);
 }
--- src/FactoryMethod/idcard/IDCard.java
+++ src/FactoryMethod/idcard/IDCard.java
@@ -1,9 +1,10 @@
 package FactoryMethod.idcard;
 import FactoryMethod.framework.*;
-
+// 인식 카드를 나타내는 클래스
 public class IDCard extends Product {
     private String owner;
 
+    // 생성자
     public IDCard(String owner) {
         System.out.print(owner + "카드 만듦\n");
         this.owner = owner;
